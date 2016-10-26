d913f47a2a656fcac0beb02f962eba40f0e7590d
thefireholder <sangjoonlee@wifi-131-179-20-188.host.ucla.edu>
2016-10-25 17:54:27 -0700

    made motor class

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
 #include "mbed.h"
 
 // motor, timer, serial port
-PwmOut MRF(PA_7);
-PwmOut MRB(PB_6);
-PwmOut MLF(PB_10);
-PwmOut MLB(PC_7);
+
 Timer t;
 Serial pc(SERIAL_TX, SERIAL_RX);
 
@@ -15,6 +12,60 @@ InterruptIn channelB(PC_4); //right encoder
 
 
 
+class Motor
+{
+public:
+    Motor(PwmOut motorF, PwmOut motorB)
+    {
+        _motorF = motorF;
+        _motorB = motorB;
+        _period = .1;
+        _motorF.period = _period;
+        _motorB.period = _period;
+    }
+    void setSpeed(float speed)
+    {
+        //if statement for positive value = forward, negative value =
+        _speed = speed;
+        if (_speed > 0)
+        {
+            _motorF = _speed;
+            _motorB = 0;
+        }
+        else if (_speed <0)
+        {
+            _motorF = 0;
+            _motorB = _speed;
+        }
+        else
+        {
+            _motorF = 0;
+            _motorB = 0;
+        }
+        
+    }
+    float getSpeed()
+    {
+        return _speed;
+    }
+    void stop()
+    {
+        _motorF = 1;
+        _motorB = 1;
+        wait(.5);
+        _motorF = 0;
+        _motorB = 0;        _
+    }
+private:
+    PwmOut _motorF, _motorB;
+    float _speed;
+    float _period;
+    int _motorF, _motorB
+};
+
+Motor RightMotor(PwmOut MRF(PA_7), PwmOut MRB(PB_6));
+Motor LeftMotor(PwmOut MLF(PB_10), PwmOut MLB(PC_7));
+
 
 DigitalOut myled(LED1);
 
@@ -24,6 +75,8 @@ void incrementEncoder()
 {
     pulses++;
 }
+
+int PID(
 
 
 int main() {
@@ -36,8 +89,8 @@ int main() {
     
     //MRF.period(.1);
     //MRF = .1;
-    MLF.period(.1);
-    MLF = .1;
+    RightMotor.setSpeed(.1); //off for .09 and on for .01
+    LeftMotor.setSpeed(.1);
     while(1){
         wait(1);
         pc.printf("Pulses is: %d \r\n", pulses);
@@ -45,3 +98,4 @@ int main() {
 
 
 }
+
