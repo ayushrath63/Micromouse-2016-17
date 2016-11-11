fce0982f6bcd422c8e483ec25bfac7a9275d4d74
Ayush Rath <ayush.rath@gmail.com>
2016-11-11 14:19:24 -0800

    Added IR Calibration funciton

--- rat_4.cpp
+++ rat_4.cpp
@@ -19,22 +19,35 @@ Ticker fourth;
 
 Serial pc(SERIAL_TX,SERIAL_RX);
 
+ int base = 0;
+
+void calibrateIR()
+{
+    for(int i = 0; i < 50; i++)
+    {
+        base+=IR_R.read();
+    }
+    base /=50;
+}
+
+
 void IR_FL_read()
 {
     IR_R = 1;
     wait(.01);
-    pc.printf("Left Forward Distance: %d \r\n", IRR_R.read());
+    pc.printf("Left Forward Distance: %d \r\n", IRR_R.read() - base);
     IR_R = 0;
 }
 
 int main()
 {
-    
+    calibrateIR();
+
     IR_FL_F.attach(&IR_FL_read, 1);
     /*second.attach(&second_func,1);
      third.attach(&third_fun,1);
      fourth.attach(&fourth_func, 1);
-     
+
      while(1)
      {
      pc.printf("Left Forward Distance: %d \r\n", IR_FL.read());
@@ -51,42 +64,42 @@ int main()
 
 
 /*IR SENSOR READING FUNCTIONS
- 
+
  // motor, timer, serial port, LED
  PwmOut MRF(PA_7);
  PwmOut MRB(PB_6);
  PwmOut MLF(PB_10);
  PwmOut MLB(PC_7);
  Timer t;
- 
+
  //IR
  AnalogIn IR_FR(PA_4);
  AnalogIn IR_FL(PC_1);
  AnalogIn IR_R(PA_0);
  AnalogIn IR_L(PC_0);
- 
+
  Ticker SecondUpdate;  //for updating speed every second
- 
+
  DigitalOut myled(LED1);
  Serial pc(SERIAL_TX,SERIAL_RX);
- 
+
  void updateSpeed();
  void forward(int duration, int speed);
- 
+
  float origianl_speed = 0.5;
  float speed = 0.5;
- 
+
  //encoder
  float IRPID();
  int prevError = 0;
  int integralError = 0; //sum of all error
- 
+
  int main()
  {
  //SecondUpdate.attach(&updateSpeed,0.2);
  t.reset();
- 
- 
+
+
  while(1)
  {
  pc.printf("Left Forward Distance: %d \r\n", IR_FL.read());
@@ -95,11 +108,11 @@ int main()
  pc.printf("Left Distance: %d \r\n\n", IR_L.read());
  wait(1.0);
  }
- 
- 
- 
+
+
+
  }
- 
+
  void forwardTill(int threshold)
  {
  while (IR_FL.read() < threshold && IR_FR.read() < threshold)
@@ -109,15 +122,15 @@ int main()
  }
  MRF = 0; MLF = 0;
  wait(3.0);
- 
+
  }
- 
+
  void updateSpeed()
  {
  MRF = speed + IRPID();
  MLF = speed - IRPID();
  }
- 
+
  float IRPID() //1 through 0.1 current if we want to stay at speed .1
  {
  float kp = (1.0/24.0) * speed * .20; // w/o PID, the error is around 24 (unit error = 24),  so 20% change in speed every unit error
@@ -130,7 +143,7 @@ int main()
  float returnVal = kp*error + ki*integralError + kd*(error - prevError);
  prevError = error;
  return returnVal;
- 
+
  }
- 
+
  */
